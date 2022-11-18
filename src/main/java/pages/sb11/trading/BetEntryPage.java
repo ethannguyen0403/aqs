c605e307878c0e8c55108b888e345212592c0526
Johnny Nguyen <johnny.nguyen@pal.net.vn>
2022-11-18 17:45:49 +0700

    - completed implementing action placeSoccerBet and openBetSlipSoccer - creating new page BetSlipPopup with capturing UI element

--- src/main/java/pages/sb11/trading/BetEntryPage.java
+++ src/main/java/pages/sb11/trading/BetEntryPage.java
@@ -6,11 +6,12 @@ import com.paltech.element.common.Label;
 import com.paltech.element.common.TextBox;
 import controls.DateTimePicker;
 import controls.Table;
+import pages.ess.popup.ColumnSettingPopup;
 import pages.sb11.WelcomePage;
+import pages.sb11.trading.popup.BetSlipPopup;
 
 public class BetEntryPage extends WelcomePage {
     Label lblTitle = Label.xpath("//div[contains(@class,'card-header')]//span[1]");
-    //label[contains(text(),'Company Unit')]
     public DropDownBox ddComp = DropDownBox.id("company-unit");
     public TextBox txtDate = TextBox.xpath("//div[@class='pl-3']/label[contains(text(),'Date')]//following::input[1]");
     public DateTimePicker dtpDate = DateTimePicker.xpath(txtDate,"//bs-days-calendar-view");
@@ -21,17 +22,64 @@ public class BetEntryPage extends WelcomePage {
     public Button btnSoccer = Button.xpath("//div[contains(@class,'card-body')]//span[contains(text(),'Soccer')]");
     public Button btnCricket = Button.xpath("//div[contains(@class,'card-body')]//span[contains(text(),'Cricket')]");
     public Button btnMixedSport = Button.xpath("//div[contains(@class,'card-body')]//span[contains(text(),'Mixed Sports')]");
+    public Table tbEvent = Table.xpath("//table[contains(@class,'table')]",16);
 
-    public void placeBetSoccer (String league, String accCode){
+    public void openBetSlipSoccer (String accCode, String betType){
         btnSoccer.click();
-        ddLeague.selectByVisibleContainsText(league);
-        btnShow.click();
+        ddLeague.selectByVisibleContainsText("All");
         txtAccCode.sendKeys(accCode);
+        btnShow.click();
+        String _betType = betType.toUpperCase();
+        switch (_betType) {
+            case "HOME-FT":
+                tbEvent.getControlOfCell(1, 4, 1, "span[1]").click();
+                return;
+            case "AWAY-FT":
+                tbEvent.getControlOfCell(1, 5, 1, "span[1]").click();
+                return;
+            case "OVER-FT":
+                tbEvent.getControlOfCell(1, 7, 1, "span[1]").click();
+                return;
+            case "UNDER-FT":
+                tbEvent.getControlOfCell(1, 8, 1, "span[1]").click();
+                return;
+            case "HOME-HT":
+                tbEvent.getControlOfCell(1, 10, 1, "span[1]").click();
+                return;
+            case "AWAY-HT":
+                tbEvent.getControlOfCell(1, 11, 1, "span[1]").click();
+                return;
+            case "OVER-HT":
+                tbEvent.getControlOfCell(1, 13, 1, "span[1]").click();
+                return;
+            case "UNDER-HT":
+                tbEvent.getControlOfCell(1, 14, 1, "span[1]").click();
+                return;
+            case "MORE":
+                tbEvent.getControlOfCell(1, 15, 1, "span[1]").click();
+                return;
+        }
     }
 
-    @Override
-    public String getTitlePage ()
-    {
-        return this.lblTitle.getText().trim();
+    public BetSlipPopup placeSoccerBet(String handicap, String odds, String stake, boolean copySameOdds, boolean copyMinusOdds, boolean placebet){
+        BetSlipPopup betSlipPopup = new BetSlipPopup();
+        betSlipPopup.ddHandicap.selectByVisibleText(handicap);
+        betSlipPopup.txtOdds.sendKeys(odds);
+        betSlipPopup.txtStake.sendKeys(stake);
+        if (copyMinusOdds){
+            betSlipPopup.cbIncreaseOdds.click();
+        }
+
+        if (copySameOdds){
+            betSlipPopup.cbSameOdds.click();
+        }
+
+        if (placebet){
+            betSlipPopup.btnPlaceBet.click();
+        }
+        return new BetSlipPopup();
     }
+    @Override
+        public String getTitlePage() {return this.lblTitle.getText().trim();}
 }
+
--- src/main/java/testcases/sb11test/BetEntryTest.java
+++ src/main/java/testcases/sb11test/BetEntryTest.java
@@ -4,22 +4,24 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.sb11.master.BookieInfoPage;
 import pages.sb11.trading.BetEntryPage;
+import pages.sb11.trading.popup.BetSlipPopup;
 import testcases.BaseCaseAQS;
 
 import static common.SBPConstants.*;
 
 public class BetEntryTest extends BaseCaseAQS {
 
-    @Test(groups = {"smoke"})
+    @Test(groups = {"smoke1"})
     public void Bookie_Info_TC_001(){
         log("@title: Verify that can login successfully");
         log("@Step 1: Login with valid account");
         log("Step 2: Click Trading > Bet Entry");
         BetEntryPage betEntryPage = welcomePage.navigatePage(TRADING, BET_ENTRY,BetEntryPage.class);
+        betEntryPage.openBetSlipSoccer("JO-TEL-AC-01","HOME-FT");
+        betEntryPage.placeSoccerBet("1.25","1","20",false,false,true);
 
-        betEntryPage.placeBetSoccer("All","JO-TEL-AC-01");
         log("Verify that Page title is correctly display");
-        //Assert.assertEquals(betEntryPage.getTitlePage(),"Trading Bet Entry","FAILED! Page Title is incorrect display");
+
         log("INFO: Executed completely");
     }
 }
