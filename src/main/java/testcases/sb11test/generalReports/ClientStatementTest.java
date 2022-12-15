e30a5126c28c02c510a96de9f0fb03aba90239af
Tim.Dang <tim.dang@pal.net.vn>
2022-12-15 09:44:12 +0700

    Implement TC C587, 588

--- src/main/java/testcases/sb11test/generalReports/ClientStatementTest.java
+++ src/main/java/testcases/sb11test/generalReports/ClientStatementTest.java
@@ -2,7 +2,6 @@ package testcases.sb11test.generalReports;
 
 import com.paltech.utils.DateUtils;
 import org.testng.Assert;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import pages.sb11.generalReports.ClientStatementPage;
 import testcases.BaseCaseAQS;
@@ -25,7 +24,7 @@ public class ClientStatementTest extends BaseCaseAQS {
         String recPayVal;
         String actualVal;
 
-        log("@title: Verify that Closing of Super = Opening + Win/Loss + Commission + Rec/Pay/CA/RB/Adj");
+        log("@title: Validate that Closing of Super = Opening + Win/Loss + Commission + Rec/Pay/CA/RB/Adj");
         log("@Step 1: Login with valid account");
         log("@Step 2: Navigate to General Reports > Client Statement");
         ClientStatementPage clientPage = welcomePage.navigatePage(GENERAL_REPORTS, CLIENT_STATEMENT, ClientStatementPage.class);
@@ -33,7 +32,7 @@ public class ClientStatementTest extends BaseCaseAQS {
         log("@Step 2: Filter a client with client point view");
         clientPage.filter(viewBy, companyUnit, financialYear, clients, "","");
 
-        log("Verify Closing of Super = Opening + Win/Loss + Commission + Rec/Pay/CA/RB/Adj");
+        log("Validate Closing of Super = Opening + Win/Loss + Commission + Rec/Pay/CA/RB/Adj");
         openingVal = clientPage.getSuperCellValue(clientPage.colOpening).replace(",","");
         winLossVal = clientPage.getSuperCellValue(clientPage.colWinLoss).replace(",","");
         commissionVal = clientPage.getSuperCellValue(clientPage.colCommission).replace(",","");
@@ -50,14 +49,10 @@ public class ClientStatementTest extends BaseCaseAQS {
     @Test(groups = {"smoke"})
     @TestRails(id = "310")
     public void ClientStatementTC_310(){
-        String viewBy = "Client Point";
-        String companyUnit = "Kastraki Limited";
-        String financialYear = "Year 2022-2023";
-        String clients = "QA_TEST - QA Client Test";
         String openingVal;
         String closingVal;
 
-        log("@title: Verify that Opening value today is Closing of yesterday");
+        log("@title: Validate that Opening value today is Closing of yesterday");
         log("@Step 1: Login with valid account");
         log("@Step 2: Navigate to General Reports > Client Statement");
         ClientStatementPage clientPage = welcomePage.navigatePage(GENERAL_REPORTS, CLIENT_STATEMENT, ClientStatementPage.class);
@@ -75,9 +70,86 @@ public class ClientStatementTest extends BaseCaseAQS {
         log("@Step 3.1: Get Closing value");
         closingVal = clientPage.getSuperCellValue(clientPage.colClosing).replace(",","");
 
-        log("@Verify that Opening value today is Closing of yesterday");
+        log("@Validate that Opening value today is Closing of yesterday");
         Assert.assertEquals(closingVal,openingVal,"FAILED! Closing Balance of previous date is not equal to Opening Balance of current date, " +
                 "Opening:"+openingVal+" and Closing:"+closingVal);
+        log("INFO: Executed completely");
+    }
+
+    @Test(groups = {"smoke"})
+    @TestRails(id = "587")
+    public void ClientStatementTC_587(){
+        String totalGrandMasterVal;
+        String totalGrandHKDVal;
+
+        log("@title: Validate total in HKD of Master match with Grand Total in HKD at bottom");
+        log("@Step 1: Login with valid account");
+        log("@Step 2: Navigate to General Reports > Client Statement");
+        ClientStatementPage clientPage = welcomePage.navigatePage(GENERAL_REPORTS, CLIENT_STATEMENT, ClientStatementPage.class);
+
+        log("@Step 2: Filter a client with client point view");
+        clientPage.filter(viewBy, companyUnit, financialYear, clients, "","");
+
+        log("Validate total in HKD of Master match with Grand Total in HKD at bottom");
+        totalGrandMasterVal = clientPage.getMasterCellValue("Total in", clientPage.colClosing).replace(",","");
+        totalGrandHKDVal = clientPage.getGrandTotal("HKD").replace(",","");
+
+        Assert.assertEquals(totalGrandMasterVal,totalGrandHKDVal,"FAILED! Grand Master value is not equal Grand HKD, Grand Master:"+totalGrandMasterVal+" and Grand HKD:"+totalGrandHKDVal);
+        log("INFO: Executed completely");
+    }
+
+    @Test(groups = {"smoke"})
+    @TestRails(id = "588")
+    public void ClientStatementTC_588(){
+        String openingVal;
+        String winLossVal;
+        String commissionVal;
+        String recPayVal;
+        String movementVal;
+        String closingVal;
+        String reversedOpeningVal;
+        String reversedWinLossVal;
+        String reversedCommissionVal;
+        String reversedRecPayVal;
+        String reversedMovementVal;
+        String reversedClosingVal;
+
+        log("@title: Validate values are the same but opposite each other when viewing by Company Point and Client Point");
+        log("@Step 1: Login with valid account");
+        log("@Step 2: Navigate to General Reports > Client Statement");
+        ClientStatementPage clientPage = welcomePage.navigatePage(GENERAL_REPORTS, CLIENT_STATEMENT, ClientStatementPage.class);
+
+        log("@Step 2: Filter a client with client point view");
+        clientPage.filter(viewBy, companyUnit, financialYear, clients, "","");
+
+        log("Verify Closing of Super = Opening + Win/Loss + Commission + Rec/Pay/CA/RB/Adj");
+        openingVal = clientPage.getSuperCellValue(clientPage.colOpening).replace(",","");
+        winLossVal = clientPage.getSuperCellValue(clientPage.colWinLoss).replace(",","");
+        commissionVal = clientPage.getSuperCellValue(clientPage.colCommission).replace(",","");
+        recPayVal = clientPage.getSuperCellValue(clientPage.colRecPay).replace(",","");
+        movementVal = clientPage.getSuperCellValue(clientPage.colMovement).replace(",","");
+        closingVal = clientPage.getSuperCellValue(clientPage.colClosing).replace(",","");
+        reversedOpeningVal = clientPage.reverseValue(openingVal);
+        reversedWinLossVal = clientPage.reverseValue(winLossVal);
+        reversedCommissionVal = clientPage.reverseValue(commissionVal);
+        reversedRecPayVal = clientPage.reverseValue(recPayVal);
+        reversedMovementVal = clientPage.reverseValue(movementVal);
+        reversedClosingVal = clientPage.reverseValue(closingVal);
+
+        clientPage.filter("Company Point", companyUnit, financialYear, clients, "","");
+        openingVal = clientPage.getSuperCellValue(clientPage.colOpening).replace(",","");
+        winLossVal = clientPage.getSuperCellValue(clientPage.colWinLoss).replace(",","");
+        commissionVal = clientPage.getSuperCellValue(clientPage.colCommission).replace(",","");
+        recPayVal = clientPage.getSuperCellValue(clientPage.colRecPay).replace(",","");
+        movementVal = clientPage.getSuperCellValue(clientPage.colMovement).replace(",","");
+        closingVal = clientPage.getSuperCellValue(clientPage.colClosing).replace(",","");
+
+        Assert.assertEquals(openingVal,reversedOpeningVal,"FAILED! Closing Balance is not calculated correctly, actual:"+openingVal+" and expected:"+reversedOpeningVal);
+        Assert.assertEquals(winLossVal,reversedWinLossVal,"FAILED! Closing Balance is not calculated correctly, actual:"+winLossVal+" and expected:"+reversedWinLossVal);
+        Assert.assertEquals(commissionVal,reversedCommissionVal,"FAILED! Closing Balance is not calculated correctly, actual:"+commissionVal+" and expected:"+reversedCommissionVal);
+        Assert.assertEquals(recPayVal,reversedRecPayVal,"FAILED! Closing Balance is not calculated correctly, actual:"+recPayVal+" and expected:"+reversedRecPayVal);
+        Assert.assertEquals(movementVal,reversedMovementVal,"FAILED! Closing Balance is not calculated correctly, actual:"+movementVal+" and expected:"+reversedMovementVal);
+        Assert.assertEquals(closingVal,reversedClosingVal,"FAILED! Closing Balance is not calculated correctly, actual:"+closingVal+" and expected:"+reversedClosingVal);
         log("INFO: Executed completely");
     }
 }
