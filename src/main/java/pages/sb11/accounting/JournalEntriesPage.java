07d112d9f4d2a2a193428c7db720355eaff0c8a7
Tim.Dang <tim.dang@pal.net.vn>
2022-12-21 11:00:37 +0700

    Implement method add txn for client/bookie

--- src/main/java/pages/sb11/accounting/JournalEntriesPage.java
+++ src/main/java/pages/sb11/accounting/JournalEntriesPage.java
@@ -7,6 +7,7 @@ import com.paltech.element.common.TextBox;
 import controls.DateTimePicker;
 import controls.Table;
 import objects.Transaction;
+import org.openqa.selenium.Keys;
 import pages.sb11.WelcomePage;
 
 public class JournalEntriesPage extends WelcomePage {
@@ -14,6 +15,10 @@ public class JournalEntriesPage extends WelcomePage {
     public DropDownBox ddCreditTo = DropDownBox.xpath("//app-transaction-creation//span[contains(text(),'Credit')]//following::select[1]");
     public DropDownBox ddDebitLedger = DropDownBox.xpath("//app-transaction-creation//span[contains(text(),'Debit')]//following::select[2]");
     public DropDownBox ddCreditLedger = DropDownBox.xpath("//app-transaction-creation//span[contains(text(),'Credit')]//following::select[2]");
+    public DropDownBox ddDebitLevel = DropDownBox.xpath("//app-transaction-creation//span[contains(text(),'Debit')]//following::select[5]");
+    public DropDownBox ddCreditLevel = DropDownBox.xpath("//app-transaction-creation//span[contains(text(),'Credit')]//following::select[5]");
+    public TextBox txtDebitAccount = TextBox.xpath("//app-transaction-creation//span[contains(text(),'Debit')]//following::input[1]");
+    public TextBox txtCreditAccount = TextBox.xpath("//app-transaction-creation//span[contains(text(),'Credit')]//following::input[1]");
     public Button btnDebitAdd = Button.xpath("//app-transaction-creation//span[contains(text(),'Debit')]//following::button[1]");
     public Button btnCreditAdd = Button.xpath("//app-transaction-creation//span[contains(text(),'Credit')]//following::button[1]");
     public TextBox txtRemark = TextBox.xpath("//app-transaction-creation//span[contains(text(),'Remark')]//following::input[1]");
@@ -26,8 +31,6 @@ public class JournalEntriesPage extends WelcomePage {
     int totalCol = 7;
     public Table tbDebit = Table.xpath("//app-transaction-creation//span[contains(text(),'Debit')]//following::table[1]",totalCol);
     public Table tbCredit = Table.xpath("//app-transaction-creation//span[contains(text(),'Credit')]//following::table[1]",totalCol);
-
-
 
     Label lblTitle = Label.xpath("//div[contains(@class,'main-box-header')]//span[1]");
     public String getTitlePage ()
@@ -67,6 +70,45 @@ public class JournalEntriesPage extends WelcomePage {
             }
         }
 
+
+    }
+
+    public void addClientBookieTransaction(Transaction trans, String fromType, boolean isSubmit){
+        filterClientBookie(fromType,true, trans.getClientDebit(), trans.getLevel(),trans.getDebitAccountCode(),true);
+        txtDebitAmount.sendKeys(String.format("%.3f",trans.getAmountDebit()));
+
+        filterClientBookie(fromType,false, trans.getClientCredit(), trans.getLevel(),trans.getCreditAccountCode(),true);
+        txtCreditAmount.sendKeys(String.format("%.3f",trans.getAmountCredit()));
+
+        txtRemark.sendKeys(trans.getRemark());
+        if (!trans.getTransDate().isEmpty()){
+            dtpTrans.selectDate(trans.getTransDate(), "dd/MM/yyyy");
+        }
+        ddTransactionType.selectByVisibleText(trans.getTransType());
+        if (isSubmit){
+            btnSubmit.click();
+        }
+    }
+
+    private void filterClientBookie(String fromType, boolean isDebit, String clientBookieCode, String level, String accountCode, boolean isAdd) {
+        if (isDebit) {
+            ddDebitFrom.selectByVisibleText(fromType);
+            ddDebitLedger.selectByVisibleText(clientBookieCode);
+            ddDebitLevel.selectByVisibleText(level);
+            txtDebitAccount.sendKeys(accountCode);
+            txtDebitAccount.sendKeys(accountCode);
+            if (isAdd) {
+                btnDebitAdd.click();
+            }
+        } else {
+            ddCreditTo.selectByVisibleText(fromType);
+            ddCreditLedger.selectByVisibleText(clientBookieCode);
+            ddCreditLevel.selectByVisibleText(level);
+            txtCreditAccount.sendKeys(accountCode);
+            if (isAdd) {
+                btnCreditAdd.click();
+            }
+        }
 
     }
 
