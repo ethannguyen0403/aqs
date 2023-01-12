799fded1a5e8505b77b0f506ca8c8588f76bb035
Johnny Nguyen <johnny.nguyen@pal.net.vn>
2023-01-12 13:18:06 +0700

    - Completed TC850

--- src/main/java/testcases/sb11test/generalReports/LedgerStatementTest.java
+++ src/main/java/testcases/sb11test/generalReports/LedgerStatementTest.java
@@ -34,7 +34,7 @@ public class LedgerStatementTest extends BaseCaseAQS {
 
     @TestRails(id="841")
     @Test(groups = {"smoke"})
-    public void Bet_Entry_TC841(){
+    public void Ledger_Statement_TC841(){
         log("@title: Validate transaction Debit of Ledger Type = Expenditure");
         log("@Step 1: Login to SB11 site");
         log("@Step 2: Navigate to Accounting > Journal Entries");
@@ -65,7 +65,7 @@ public class LedgerStatementTest extends BaseCaseAQS {
 
     @TestRails(id="842")
     @Test(groups = {"smoke"})
-    public void Bet_Entry_TC842(){
+    public void Ledger_Statement_TC842(){
         log("@title: Validate transaction Credit of Ledger Type = Expenditure");
         log("@Step 1: Login to SB11 site");
         log("@Step 2: Navigate to Accounting > Journal Entries");
@@ -96,7 +96,7 @@ public class LedgerStatementTest extends BaseCaseAQS {
 
     @TestRails(id="843")
     @Test(groups = {"smoke"})
-    public void Bet_Entry_TC843(){
+    public void Ledger_Statement_TC843(){
         log("@title: Validate value calculated correctly for Ledger Type = Expenditure (Debit)");
         log("@Step 1: Login to SB11 site");
         log("@Step 2: Navigate to Accounting > Journal Entries");
@@ -131,7 +131,7 @@ public class LedgerStatementTest extends BaseCaseAQS {
 
     @TestRails(id="844")
     @Test(groups = {"smoke"})
-    public void Bet_Entry_TC844(){
+    public void Ledger_Statement_TC844(){
         log("@title: Validate value calculated correctly for Ledger Type = Expenditure (Credit)");
         log("@Step 1: Login to SB11 site");
         log("@Step 2: Navigate to Accounting > Journal Entries");
@@ -165,7 +165,7 @@ public class LedgerStatementTest extends BaseCaseAQS {
 
     @TestRails(id="845")
     @Test(groups = {"smoke"})
-    public void Bet_Entry_TC845(){
+    public void Ledger_Statement_TC845(){
         log("@title: Validate transaction Debit of Ledger Type = Asset");
         log("@Step 1: Login to SB11 site");
         log("@Step 2: Navigate to Accounting > Journal Entries");
@@ -196,7 +196,7 @@ public class LedgerStatementTest extends BaseCaseAQS {
 
     @TestRails(id="846")
     @Test(groups = {"smoke"})
-    public void Bet_Entry_TC846(){
+    public void Ledger_Statement_TC846(){
         log("@title: Validate transaction Credit of Ledger Type = Asset");
         log("@Step 1: Login to SB11 site");
         log("@Step 2: Navigate to Accounting > Journal Entries");
@@ -227,7 +227,7 @@ public class LedgerStatementTest extends BaseCaseAQS {
 
     @TestRails(id="847")
     @Test(groups = {"smoke"})
-    public void Bet_Entry_TC847(){
+    public void Ledger_Statement_TC847(){
         log("@title: Validate value calculated correctly for Ledger Type = Asset (Debit)");
         log("@Step 1: Login to SB11 site");
         log("@Step 2: Navigate to Accounting > Journal Entries");
@@ -262,7 +262,7 @@ public class LedgerStatementTest extends BaseCaseAQS {
 
     @TestRails(id="848")
     @Test(groups = {"smoke"})
-    public void Bet_Entry_TC848(){
+    public void Ledger_Statement_TC848(){
         log("@title: Validate value calculated correctly for Ledger Type = Asset (Credit)");
         log("@Step 1: Login to SB11 site");
         log("@Step 2: Navigate to Accounting > Journal Entries");
@@ -297,7 +297,7 @@ public class LedgerStatementTest extends BaseCaseAQS {
 
     @TestRails(id="849")
     @Test(groups = {"smoke1"})
-    public void Bet_Entry_TC849(){
+    public void Ledger_Statement_TC849(){
         log("@title: Validate transaction Debit of Ledger Type = Liability");
         log("@Step 1: Login to SB11 site");
         log("@Step 2: Navigate to Accounting > Journal Entries");
@@ -325,4 +325,37 @@ public class LedgerStatementTest extends BaseCaseAQS {
         ledgerStatementPage.verifyLedgerTrans(transaction, true, lgLiabilityGroup);
         log("INFO: Executed completely");
     }
+
+    @TestRails(id="850")
+    @Test(groups = {"smoke1"})
+    public void Ledger_Statement_TC850(){
+        log("@title: Validate transaction Credit of Ledger Type = Liability");
+        log("@Step 1: Login to SB11 site");
+        log("@Step 2: Navigate to Accounting > Journal Entries");
+        JournalEntriesPage journalEntriesPage = welcomePage.navigatePage(ACCOUNTING,JOURNAL_ENTRIES,JournalEntriesPage.class);
+        log("@Step 3: In Debit, select From = Ledger, Ledger = ledger account at precondition then click Add");
+        Transaction transaction = new Transaction.Builder()
+                .ledgerDebit(debitLibAcc)
+                .ledgerCredit(creditLibAcc)
+                .ledgerDebitCur(lgDebitCur)
+                .ledgerCreditCur(lgCreditCur)
+                .amountDebit(1)
+                .amountCredit(1)
+                .remark(descLiability)
+                .transDate("")
+                .transType(transType)
+                .build();
+        log("@Step 4: Input Amount for Debit and Credit (should be same e.g 10)");
+        log("@Step 5: Choose Transaction Type = any and click Submit");
+        journalEntriesPage.addLedgerTransaction(transaction,true);
+        log("@Step 6: Navigate to General > Ledger Statement and search the transaction of ledger at precondition");
+        LedgerStatementPage ledgerStatementPage = welcomePage.navigatePage(GENERAL_REPORTS,LEDGER_STATEMENT,LedgerStatementPage.class);
+        ledgerStatementPage.showLedger(companyUnit,financialYear,"Liability",lgLiabilityGroup,"","");
+        log("@Verify 1: Original Currency: Ledger column with Ledger Group and Ledger Name, CUR column with ledger currency, Credit/Debit column = value inputted at step 5 in blue, Running Bal and Running Bal CT displayed");
+        log("@Verify 2: Amounts in GBP (conver to GBP): Credit/Debit column =  value inputted at step 5 in blue , Running Bal get value from Original Currency");
+        ledgerStatementPage.verifyLedgerTrans(transaction, false, lgLiabilityGroup);
+        log("INFO: Executed completely");
+    }
+
+
 }
