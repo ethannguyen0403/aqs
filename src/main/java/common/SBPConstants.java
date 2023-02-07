71240c0f260fd489a091a13abf85cc4cabcde8c5
isabella.huynh <isabella.huynh@pal.net.vn>
2023-02-07 22:20:10 +0700

    check file is commited

--- src/main/java/common/SBPConstants.java
+++ src/main/java/common/SBPConstants.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 public class SBPConstants {
+
     public final static String MASTER = "Master";
     public final static String BOOKIE_INFO = "Bookie Info";
     public final static String CLIENT_SYSTEM = "Client System";
