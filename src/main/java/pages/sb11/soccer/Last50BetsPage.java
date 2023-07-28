9814d316388d7760b3429beecb6e1754b1615e77
Tim.Dang <tim.dang@pal.net.vn>
2023-07-28 18:17:22 +0700

    Implement TCs of BBT

---
+++ src/main/java/pages/sb11/soccer/Last50BetsPage.java
@@ -0,0 +1,8 @@
+package pages.sb11.soccer;
+
+import com.paltech.element.common.Label;
+
+public class Last50BetsPage {
+    public Label lblHeaderGroup = Label.xpath("//app-pending-bets//table[@aria-label='table']//thead//th");
+
+}
