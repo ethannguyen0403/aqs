0b1a440e9de86b157ea3aa041ea47975d6b945a0
Ethan Nguyen <Ethan.Nguyen@pal.net.vn>
2024-04-26 14:48:42 +0700

    Stabilize 2.0

--- src/main/java/pages/sb11/master/ClientSystemPage.java
+++ src/main/java/pages/sb11/master/ClientSystemPage.java
@@ -5,12 +5,17 @@ import com.paltech.element.common.Button;
 import com.paltech.element.common.DropDownBox;
 import com.paltech.element.common.Label;
 import com.paltech.element.common.TextBox;
+import common.SBPConstants;
 import controls.Table;
+import org.testng.Assert;
 import pages.sb11.WelcomePage;
 import pages.sb11.master.clientsystempopup.AccountListPopup;
 import pages.sb11.master.clientsystempopup.AgentListPopup;
 import pages.sb11.master.clientsystempopup.MasterListPopup;
 import pages.sb11.master.clientsystempopup.MemberListPopup;
+import utils.sb11.CompanySetUpUtils;
+
+import static common.SBPConstants.COMPANY_UNIT_LIST;
 
 public class ClientSystemPage extends WelcomePage {
     Label lblTitle = Label.xpath("//div[contains(@class,'card-header')]//span[1]");
@@ -23,7 +28,6 @@ public class ClientSystemPage extends WelcomePage {
     public DropDownBox ddpClientList = DropDownBox.xpath("//label[text()='Client List']//following::select[1]");
     public DropDownBox ddpCurrency = DropDownBox.xpath("//label[text()='Currency']//following::select[1]");
     public DropDownBox ddpStatus = DropDownBox.xpath("//label[text()='Status']//following::select[1]");
-
     public Label lblClientName = Label.xpath("//label[text()='Client Name']");
     public TextBox txtClientName = TextBox.id("name");
 
@@ -34,12 +38,8 @@ public class ClientSystemPage extends WelcomePage {
     public Table tbClient = Table.xpath("//div[@class='d-inline-block col-6']//table",6);
     public Table tbSuperMaster = Table.xpath("//div[@class='col-6 d-inline-block']//table",10);
     int colClientName = 3;
-    int colXButton = 4;
     int colMaster = 5;
     int colAgent = 6;
-    int colMember = 7;
-    int colList = 10;
-
     public void filterClient(String companyUnit, String clientList, String clientName, String currency, String status) {
         if (!companyUnit.isEmpty())
             ddpCompanyUnit.selectByVisibleText(companyUnit);
@@ -56,7 +56,7 @@ public class ClientSystemPage extends WelcomePage {
     }
 
     public boolean isClientCodeExist(String clientCode){
-        int rowIndex = getClientCodeRowIndex(clientCode);
+        int rowIndex = tbClient.getRowIndexContainValue(clientCode,tbClient.getColumnIndexByName("Client Name"),null);
         String client = tbClient.getControlOfCell(1,colClientName,rowIndex,null).getText();
         if (client.contains(clientCode))
             return true;
@@ -66,70 +66,39 @@ public class ClientSystemPage extends WelcomePage {
     public void exportClientList(){
         btnExportToExcel.scrollToTop();
         btnExportToExcel.click();
+        waitSpinnerDisappeared();
     }
 
     public MasterListPopup openMasterList(String clientCode){
-        int rowIndex = getClientCodeRowIndex(clientCode);
+        int rowIndex = tbClient.getRowIndexContainValue(clientCode,tbClient.getColumnIndexByName("Client Name"),null);
         tbSuperMaster.getControlOfCell(1,colMaster,rowIndex,null).click();
         waitSpinnerDisappeared();
         return new MasterListPopup();
     }
 
     public AgentListPopup openAgentList(String clientCode){
-        int rowIndex = getClientCodeRowIndex(clientCode);
+        int rowIndex = tbClient.getRowIndexContainValue(clientCode,tbClient.getColumnIndexByName("Client Name"),null);
         tbSuperMaster.getControlOfCell(1,colAgent,rowIndex,null).click();
         return new AgentListPopup();
     }
 
     public AccountListPopup openAccountList(String clientCode){
-        int rowIndex = getClientCodeRowIndex(clientCode);
+        int rowIndex = tbClient.getRowIndexContainValue(clientCode,tbClient.getColumnIndexByName("Client Name"),null);
         tbSuperMaster.getControlOfCell(1, tbSuperMaster.getColumnIndexByName("#Memb"),rowIndex,null).click();
         waitSpinnerDisappeared();
         return new AccountListPopup();
     }
 
     public MemberListPopup openMemberList(String clientCode){
-        int rowIndex = getClientCodeRowIndex(clientCode);
-        tbSuperMaster.getControlOfCell(1,colList,rowIndex,null).click();
+        int rowIndex = tbClient.getRowIndexContainValue(clientCode,tbClient.getColumnIndexByName("Client Name"),null);
+        tbSuperMaster.getControlOfCell(1,tbSuperMaster.getColumnIndexByName("List"),rowIndex,null).click();
         waitSpinnerDisappeared();
         return new MemberListPopup();
     }
 
     public BaseElement getControlXButton(String superCode) {
-        int rowIndex = getSupeMasterCodeRowIndex(superCode);
-        return tbSuperMaster.getControlOfCell(1, colXButton, rowIndex, null);
-    }
-
-    private int getClientCodeRowIndex(String clientCode){
-        int i = 1;
-        Label lblClientCode;
-        int indexCol = tbClient.getColumnIndexByName("Client Name");
-        while (true){
-            lblClientCode = Label.xpath(tbClient.getxPathOfCell(1,indexCol,i,null));
-            if(!lblClientCode.isDisplayed()){
-                System.out.println("The client code "+clientCode+" does not display in the list");
-                return 0;
-            }
-            if(lblClientCode.getText().contains(clientCode))
-                return i;
-            i = i +1;
-        }
-    }
-
-    private int getSupeMasterCodeRowIndex(String superCode) {
-        int i = 1;
-        int colIndex = tbSuperMaster.getColumnIndexByName("Super Master");
-        Label lblClientCode;
-        while (true) {
-            lblClientCode = Label.xpath(tbSuperMaster.getxPathOfCell(1, colIndex, i, null));
-            if (!lblClientCode.isDisplayed()) {
-                System.out.println("The master code " + superCode + " does not display in the list");
-                return 0;
-            }
-            if (lblClientCode.getText().contains(superCode))
-                return i;
-            i = i + 1;
-        }
+        int rowIndex = tbSuperMaster.getRowIndexContainValue(superCode,tbSuperMaster.getColumnIndexByName("Super Master"),null);
+        return tbSuperMaster.getControlOfCell(1, tbSuperMaster.getColumnIndexByName(""), rowIndex, null);
     }
 
     public boolean verifyElementIsDisabled(BaseElement element, String attribute){
@@ -138,5 +107,22 @@ public class ClientSystemPage extends WelcomePage {
 
     public String getTooltipText(BaseElement element){
         return element.getAttribute("title").trim();
+    }
+
+    public void verifyUI() {
+        System.out.println("Dropdown: Company Unit, Support By, Currency, Status");
+        Assert.assertEquals(ddpCompanyUnit.getOptions(), CompanySetUpUtils.getListCompany(),"Failed! Company Unit dropdown is not displayed!");
+        Assert.assertEquals(ddpClientList.getOptions(), SBPConstants.ClientSystem.CLIENT_LIST,"Failed! Client List dropdown is not displayed!");
+        Assert.assertEquals(ddpCurrency.getOptions(), SBPConstants.ClientSystem.CURRENCY_LIST,"Failed! Currency dropdown is not displayed!");
+        Assert.assertEquals(ddpStatus.getOptions(), SBPConstants.ClientSystem.STATUS_LIST,"Failed! Status dropdown is not displayed!");
+        System.out.println("Textbox: Client Name");
+        Assert.assertEquals(lblClientName.getText(),"Client Name","Failed! Client Name textbox is not displayed!");
+        System.out.println("Button: Show, Add Bookie, Export To Excel");
+        Assert.assertEquals(btnShow.getText(),"Show","Failed! Show button is not displayed!");
+        Assert.assertEquals(btnAddClient.getText(),"Add Client","Failed! Add Client button is not displayed!");
+        Assert.assertEquals(btnExportToExcel.getText(),"Export To Excel","Failed! Export To Excel button is not displayed!");
+        System.out.println("Validate Client System table is displayed with correct header");
+        Assert.assertEquals(tbClient.getHeaderNameOfRows(), SBPConstants.ClientSystem.TABLE_HEADER_CLIENT,"Failed! Client table header is displayed incorrectly!!");
+        Assert.assertEquals(tbSuperMaster.getHeaderNameOfRows(), SBPConstants.ClientSystem.TABLE_HEADER_SUPER_MASTER,"Failed! Super Master table header is displayed incorrectly!!");
     }
 }
--- src/main/java/testcases/sb11test/master/ClientSystemTest.java
+++ src/main/java/testcases/sb11test/master/ClientSystemTest.java
@@ -30,7 +30,7 @@ public class ClientSystemTest extends BaseCaseAQS {
         log("INFO: Executed completely");
     }
 
-    @Test(groups = {"regression"})
+    @Test(groups = {"regression","ethan2.0"})
     @TestRails(id = "2207")
     public void Client_System_2207(){
         log("@title: Validate UI on Client System is correctly displayed");
@@ -38,20 +38,7 @@ public class ClientSystemTest extends BaseCaseAQS {
         log("Step 2: Click Master > Client System");
         ClientSystemPage clientSystemPage = welcomePage.navigatePage(MASTER, CLIENT_SYSTEM,ClientSystemPage.class);
         log("Validate UI on Client System is correctly displayed");
-        log("Dropdown: Company Unit, Support By, Currency, Status");
-        Assert.assertEquals(clientSystemPage.ddpCompanyUnit.getOptions(),COMPANY_UNIT_LIST,"Failed! Company Unit dropdown is not displayed!");
-        Assert.assertEquals(clientSystemPage.ddpClientList.getOptions(),ClientSystem.CLIENT_LIST,"Failed! Client List dropdown is not displayed!");
-        Assert.assertEquals(clientSystemPage.ddpCurrency.getOptions(),ClientSystem.CURRENCY_LIST,"Failed! Currency dropdown is not displayed!");
-        Assert.assertEquals(clientSystemPage.ddpStatus.getOptions(),ClientSystem.STATUS_LIST,"Failed! Status dropdown is not displayed!");
-        log("Textbox: Client Name");
-        Assert.assertEquals(clientSystemPage.lblClientName.getText(),"Client Name","Failed! Client Name textbox is not displayed!");
-        log("Button: Show, Add Bookie, Export To Excel");
-        Assert.assertEquals(clientSystemPage.btnShow.getText(),"Show","Failed! Show button is not displayed!");
-        Assert.assertEquals(clientSystemPage.btnAddClient.getText(),"Add Client","Failed! Add Client button is not displayed!");
-        Assert.assertEquals(clientSystemPage.btnExportToExcel.getText(),"Export To Excel","Failed! Export To Excel button is not displayed!");
-        log("Validate Client System table is displayed with correct header");
-        Assert.assertEquals(clientSystemPage.tbClient.getHeaderNameOfRows(),ClientSystem.TABLE_HEADER_CLIENT,"Failed! Client table header is displayed incorrectly!!");
-        Assert.assertEquals(clientSystemPage.tbSuperMaster.getHeaderNameOfRows(),ClientSystem.TABLE_HEADER_SUPER_MASTER,"Failed! Super Master table header is displayed incorrectly!!");
+        clientSystemPage.verifyUI();
         log("INFO: Executed completely");
     }
 
@@ -84,7 +71,7 @@ public class ClientSystemTest extends BaseCaseAQS {
         clientSystemPage.exportClientList();
         log("Validate can export Client List to Excel file successfully with exported file name: bookie_list");
         Assert.assertTrue(FileUtils.doesFileNameExist(downloadPath), "Failed to download Expected document");
-
+s
         log("@Post-condition: delete download file");
         // try {
         //     FileUtils.removeFile(downloadPath);
@@ -139,7 +126,7 @@ public class ClientSystemTest extends BaseCaseAQS {
         log("INFO: Executed completely");
     }
 
-    @Test(groups = {"regression"})
+    @Test(groups = {"regression","ethan2.0"})
     @TestRails(id = "2213")
     @Parameters({"clientCode"})
     public void Client_System_2213(String clientCode){
@@ -154,10 +141,10 @@ public class ClientSystemTest extends BaseCaseAQS {
         log("INFO: Executed completely");
     }
 
-    @Test(groups = {"regression", "2023.12.29"})
+    @Test(groups = {"regression", "2023.12.29","ethan2.0"})
     @TestRails(id = "16171")
-    @Parameters({"clientCode", "superCode"})
-    public void Client_System_16171(String clientCode, String superCode){
+    @Parameters({"clientCode"})
+    public void Client_System_16171(String clientCode){
         log("@title: Validate X button is disabled when having transaction on Client System");
         log("@Precondition: Having Client account that already had transaction: " + clientCode);
         log("@Step 1: Login with valid account");
@@ -166,14 +153,14 @@ public class ClientSystemTest extends BaseCaseAQS {
         log("@Step 3: Filter with client code: " + clientCode);
         clientSystemPage.filterClient(KASTRAKI_LIMITED, "", clientCode, "", "");
         log("@Verify 1: Validate X button is disabled when having transaction on Bookie Info");
-        Assert.assertTrue(clientSystemPage.verifyElementIsDisabled(clientSystemPage.getControlXButton(superCode), "class"), "FAILED! X button is enabled");
+        Assert.assertTrue(clientSystemPage.verifyElementIsDisabled(clientSystemPage.getControlXButton("QA2112"), "class"), "FAILED! X button is enabled");
         log("INFO: Executed completely");
     }
 
-    @Test(groups = {"regression", "2023.12.29"})
+    @Test(groups = {"regression", "2023.12.29","ethan2.0"})
     @TestRails(id = "16172")
-    @Parameters({"clientCode", "superCode"})
-    public void Client_System_16172(String clientCode, String superCode){
+    @Parameters({"clientCode"})
+    public void Client_System_16172(String clientCode){
         log("@title: Validate tooltip is displayed correctly when hovering on X button on Client System");
         log("@Precondition: Having Client account that already had transaction: " + clientCode);
         log("@Step 1: Login with valid account");
@@ -181,9 +168,9 @@ public class ClientSystemTest extends BaseCaseAQS {
         ClientSystemPage clientSystemPage = welcomePage.navigatePage(MASTER, CLIENT_SYSTEM,ClientSystemPage.class);
         log("@Step 3: Filter with client code: " + clientCode);
         clientSystemPage.filterClient(KASTRAKI_LIMITED, "", clientCode, "", "");
-        log("@Step 4: Hover on x button of Super master " + superCode);
+        log("@Step 4: Hover on x button of Super master QA2112");
         log("@Verify 1: Validate tooltip is displayed correct when hovering on X button");
-        Assert.assertEquals(clientSystemPage.getTooltipText(clientSystemPage.getControlXButton(superCode)),
+        Assert.assertEquals(clientSystemPage.getTooltipText(clientSystemPage.getControlXButton("QA2112")),
                 String.format(BookieSystem.TOOLTIP_MESSAGE, "Super Master"), "FAILED! Text on tool tip is not correct");
         log("INFO: Executed completely");
     }
